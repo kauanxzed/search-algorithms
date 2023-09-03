2e57ba97eef64c07341eaecda46e7e1973fa8375
Kauan Valerio <kauanxzed@gmail.com>
2023-09-03 12:59:36 -0300

    feat: 🎸 making select component

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,8 +1,10 @@
 import Button from '../button/button';
+import Select from '../select/select';
 
 export function Header() {
   return (
     <div className="border-spacing-1 bg-slate-800 p-10 fixed w-full">
+      <Select />
       <Button className="float-right" msg="Do the sorting" />
     </div>
   );
--- src/components/select/select.tsx
+++ src/components/select/select.tsx
@@ -1,12 +1,66 @@
-import styles from './select.module.css';
+import { Listbox } from '@headlessui/react';
+import { useState } from 'react';
+import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid';
 
-/* eslint-disable-next-line */
-export interface SelectProps {}
+const sortingTypes = [
+  { name: 'Booble Sort' },
+  { name: 'Quick Sort' },
+  { name: 'Merge Sort' },
+  { name: 'Heap Sort' },
+];
 
-export function Select(props: SelectProps) {
+export interface SelectProps {
+  options?: string[];
+  className?: string;
+}
+
+export function Select() {
+  const [selected, setSelected] = useState(sortingTypes[0]);
   return (
-    <div className={styles['container']}>
-      <h1>Welcome to Select!</h1>
+    <div className="float-left w-72">
+      <Listbox value={selected} onChange={setSelected}>
+        <div className="relative">
+          <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-3 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
+            <span className="block truncate">{selected.name}</span>
+            <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+              <ChevronUpDownIcon
+                className="h-5 w-5 text-gray-400"
+                aria-hidden="true"
+              />
+            </span>
+          </Listbox.Button>
+          <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+            {sortingTypes.map((sortingType, sortingTypeIdx) => (
+              <Listbox.Option
+                key={sortingTypeIdx}
+                className={({ active }) =>
+                  `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                    active ? 'bg-teal-100 text-teal-900' : 'text-gray-900'
+                  }`
+                }
+                value={sortingType}
+              >
+                {({ selected }) => (
+                  <div>
+                    <span
+                      className={`block truncate ${
+                        selected ? 'font-medium' : 'font-normal'
+                      }`}
+                    >
+                      {sortingType.name}
+                    </span>
+                    {selected ? (
+                      <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-teal-600">
+                        <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                      </span>
+                    ) : null}
+                  </div>
+                )}
+              </Listbox.Option>
+            ))}
+          </Listbox.Options>
+        </div>
+      </Listbox>
     </div>
   );
 }
